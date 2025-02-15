3a2aeadc9ed2c3c14612d7945c2a8abb7ace59fb
mohammed7subhan <malekmohammedsubhan@gmail.com>
2025-02-15 18:07:56 -0500

    Add files via upload

---
+++ lab0.c
@@ -0,0 +1,23 @@
+/***************************************************************** 
+
+    File: lab0.c
+    Author: [Your Name]
+    Student Number: [Your Student Number]
+
+    To compile program in codespaces, in terminal pane type:
+        gcc -Wall lab0.c
+    To run program in codespaces, in terminal pane type:
+        ./a.out
+        
+***************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("My name is mohammed subhan.\n");
+    printf("I am a student.\n");
+    printf("I also love playing soccer for fun.\n");
+
+    return 0;
+}
